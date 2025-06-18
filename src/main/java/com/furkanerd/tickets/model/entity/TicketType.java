d041559f4417c28d46cf840192e4dd8ebc060f58
furkanerdogan <furkanerdd1@gmail.com>
2025-06-18 15:20:12 +0300

    feat: add entity classes and relations

---
+++ src/main/java/com/furkanerd/tickets/model/entity/TicketType.java
@@ -0,0 +1,65 @@
+package com.furkanerd.tickets.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "ticket_types")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TicketType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id",updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "total_available")
+    private Integer totalAvailable;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    //TODO : tickets
+    @OneToMany(mappedBy = "ticketType" , cascade = CascadeType.ALL)
+    private List<Ticket> tickets =  new ArrayList<>();
+
+    @CreatedDate
+    @Column(name = "created_at",updatable = false ,nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at",nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TicketType that = (TicketType) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(totalAvailable, that.totalAvailable) && Objects.equals(price, that.price) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, totalAvailable, price, createdAt, updatedAt);
+    }
+}
