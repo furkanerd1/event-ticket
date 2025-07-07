841557ab3009c615e060cc0f07f179745be625d8
furkanerdogan <furkanerdd1@gmail.com>
2025-07-07 16:03:56 +0300

    fix auth error

--- src/main/java/com/furkanerd/tickets/config/SecurityConfig.java
+++ src/main/java/com/furkanerd/tickets/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/api/v1/published-events/**").permitAll()
                                 .requestMatchers("/api/v1/events").hasRole("ORGANIZER")
+                                .requestMatchers("/api/v1/ticket-validations").hasRole("STAFF")
                                 .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
